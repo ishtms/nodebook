31e25d2b25688f6d8071277f0a1c0d3b35e91004
Ishtmeet <ish.rissam@gmail.com>
2025-09-06 14:01:45 +0530

    chapter: 1.3 - the event loop ready for early access + mark as hidden

--- src/content/node-arch/_meta.js
+++ src/content/node-arch/_meta.js
@@ -1,5 +1,8 @@
 export default {
   "what-is-nodejs": "What Node.js Actually Is ",
   "v8-engine-intro": "Inside the v8 engine",
-  "event-loop-intro": "The Node.js Event Loop",
+  "event-loop-intro": {
+    display: "hidden",
+    title: "The Node.js Event Loop",
+  },
 };
